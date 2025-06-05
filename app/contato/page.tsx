3755fe6e8df18229132d7a2a8b4ec4ec5ce79d7c
guledev <gule.dev64@gmail.com>
2025-06-05 13:55:02 -0300

    feat/Create nav bar component and dependency pages

---
+++ app/contato/page.tsx
@@ -0,0 +1,7 @@
+export default function contato() {
+    return (
+        <main>
+            Contato
+        </main>
+    )
+}
---
+++ app/localizacao/page.tsx
@@ -0,0 +1,7 @@
+export default function localizacao() {
+    return (
+        <main>
+            Localização
+        </main>
+    )
+}
