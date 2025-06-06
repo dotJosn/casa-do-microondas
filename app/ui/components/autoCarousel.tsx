7f92b367655dfb7507a19b608310512bb5fe1577
guledev <gule.dev64@gmail.com>
2025-06-06 09:04:17 -0300

    update/next removed

--- app/ui/components/autoCarousel.tsx
+++ app/ui/components/autoCarousel.tsx
@@ -26,8 +26,6 @@ export default function AutoCarosel(props: any) {
                     ))
                 }
                 </CarouselContent>
-                <CarouselPrevious />
-                <CarouselNext />
             </Carousel>
         </div>
     )
